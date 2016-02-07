30faee57a57ce1de85ded3efc36ebc9e501a2a48
preethianantharaman <preethi.anantharaman@sjsu.edu>
2016-02-06 17:36:36 -0800

    Initial commit

---
+++ Application.java
@@ -0,0 +1,39 @@
+package LearnJava;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+	int myNumber = 88;
+	short myShort = 234;
+	long myLong	= 3333333;
+	
+	double myDouble = 3.14;
+	float myFloat = 3.141f;
+	
+	char myChar = 'y';
+	boolean myBoolean = true;
+	
+	byte myByte = 127;
+	
+	String text = "hello";
+	String blank = " ";
+	String name = "Preethi";
+	String greeting = text + blank + name;
+	
+	System.out.println(greeting);
+	System.out.println("Hello" + " " + "Preethi" );
+	// My First Line of Code	
+	System.out.println("Hello Prety");
+	System.out.println("My    No:" + myNumber);
+	System.out.println("Short No:" + myShort);
+	System.out.println("Long  No:" + myLong);
+	System.out.println("DoubleNo:" + myDouble);
+	System.out.println("Float NO:" + myFloat);
+	System.out.println("Character:" + myChar);
+	System.out.println("Boolean :" + myBoolean);
+	System.out.println("Byte 	:" + myByte);
+		
+	}
+
+}
